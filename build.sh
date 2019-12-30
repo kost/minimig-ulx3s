c53ce0cd1c6b837e516477198b09358ee7f59c42
Vlatko Kosturjak <kost@linux.hr>
2019-12-30 14:22:01 +0100

    use new version of build tools

--- build.sh
+++ build.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-export ulx3s_url=https://github.com/alpin3/ulx3s/releases/download/v2019.12.26/ulx3s-2019.12.26-linux-x86_64.tar.gz
-export ulx3s_tmp=/tmp/ulx3s.tgz
+export ulx3s_url=https://github.com/alpin3/ulx3s/releases/download/v2019.12.30/ulx3s-2019.12.30-linux-x86_64.tar.gz
 export ulx3s_dist=/dist/
 
  yum update -y ; \
  yum install -y python3 zip unzip make git && \
- wget $ulx3s_url -O $ulx3s_tmp && \
- tar -xvz -C /opt -f $ulx3s_tmp && \
+ curl -L $ulx3s_url | tar -xvz -C /opt -f - && \
  ln -sf /opt/ulx3s-* /opt/ulx3s && \
- rm -f $ulx3s_tmp && \
  mkdir -p /mt/scratch/tmp/openfpga/prjtrellis/libtrellis/ && \
  cd /opt/ulx3s/bin && \
  for i in ecp*; do ln -sf `pwd`/$i /mt/scratch/tmp/openfpga/prjtrellis/libtrellis/$i; done && \
